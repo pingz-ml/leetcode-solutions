968b3c8b968edb5d8b9b325ec61569abbd5817db
Ping Zhao <ping@zhao.ml>
2025-02-11 22:17:17 +0100

    Add a solution to 3110 Score of a String

---
+++ 3110_score_of_a_string/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <numeric>
+
+void init() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+class Solution {
+public:
+
+    int scoreOfString(const std::string& s) {
+        init();
+
+        int sum = 0;
+        for (
+            std::string::const_iterator p = s.begin(), q = p + 1;
+            q != s.end();
+            ++p, ++q
+        ) {
+            sum += std::abs(*p - *q);
+        }
+
+        return sum;
+    }
+};
